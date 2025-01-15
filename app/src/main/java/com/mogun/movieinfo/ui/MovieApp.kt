589b8a9d921f1a8e63346d0a310d0a1891a9652e
seungohjin <corrobolea@gmail.com>
2025-01-15 20:42:49 +0900

    update: BottomNavigation 추가 및 Home, Search, Watch Screen 화면 연결

--- app/src/main/java/com/mogun/movieinfo/ui/MovieApp.kt
+++ app/src/main/java/com/mogun/movieinfo/ui/MovieApp.kt
@@ -1,13 +1,27 @@
 package com.mogun.movieinfo.ui
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.mogun.movieinfo.core.ui.compose.LocalSnackbarHostState
 import com.mogun.movieinfo.core.ui.compose.MovieAppProvider
+import com.mogun.movieinfo.ui.navigation.NavigationItem
+import com.mogun.movieinfo.ui.screen.HomeScreen
+import com.mogun.movieinfo.ui.screen.SearchScreen
+import com.mogun.movieinfo.ui.screen.WatchScreen
+import com.mogun.movieinfo.ui.theme.MovieInfoTheme
 
 @Composable
 fun MovieApp(
@@ -16,25 +30,54 @@ fun MovieApp(
     MovieAppProvider {
         MovieAppScreen(modifier)
     }
-
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun MovieAppScreen(
     modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
 ) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
     val snackbarHostState = LocalSnackbarHostState.current
 
     Scaffold(
         topBar = {
-
+            TopAppBar(
+                title = {
+                    Text("Movie App")
+                }
+            )
         },
         snackbarHost = {
             SnackbarHost(
                 hostState = snackbarHostState,
             )
+        },
+        bottomBar = {
+            MovieAppBottomBar(
+                navHostController = navController,
+                currentRoute = currentDestination?.route,
+            )
         }
     ) {
-        Text("Movie App", modifier = modifier.padding(it))
+        NavHost(
+            navController = navController,
+            startDestination = NavigationItem.Home.route,
+            modifier = modifier.padding(it)
+        ) {
+            composable(NavigationItem.Home.route) { HomeScreen() }
+            composable(NavigationItem.Search.route) { SearchScreen() }
+            composable(NavigationItem.Watch.route) { WatchScreen() }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MovieAppPreview() {
+    MovieInfoTheme {
+        MovieApp()
     }
 }
---
+++ app/src/main/java/com/mogun/movieinfo/ui/navigation/NavigationItem.kt
@@ -0,0 +1,29 @@
+package com.mogun.movieinfo.ui.navigation
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class NavigationItem(
+    val title: String,
+    val route: String,
+    val icon: ImageVector,
+) {
+    object Home : NavigationItem(
+        title = "Home",
+        route = "home",
+        icon = Icons.Default.Home,
+    )
+    object Search : NavigationItem(
+        title = "Search",
+        route = "search",
+        icon = Icons.Default.Search,
+    )
+    object Watch : NavigationItem(
+        title = "Watch",
+        route = "watch",
+        icon = Icons.Default.Favorite,
+    )
+}
